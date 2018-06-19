80ed314971ea05af123848fb51db0328c100f723
manitaKHO <manita.kho@gmail.com>
2018-06-19 11:53:21 +0700

    Create Project

---
+++ mu-Wallet/App_Code/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for User
+/// </summary>
+public class User
+{
+    string username { get; set; }
+    public User()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}
